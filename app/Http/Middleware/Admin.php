e30c33dc4d4e43a9c7a3ea77ee1b273bb4b21593
Ichlasul1744 <56462872+ichlasul1744@users.noreply.github.com>
2024-03-27 15:27:16 +0700

    [FEATURE] Product

--- app/Http/Middleware/Admin.php
+++ app/Http/Middleware/Admin.php
@@ -1,13 +1,12 @@
 <?php
-
+ 
 namespace App\Http\Middleware;
-
+ 
 use Closure;
 use Illuminate\Http\Request;
-use Illuminate\support\Facades\Auth;
-use Illuminate\Support\Facades\Redirect;
+use Illuminate\Support\Facades\Auth;
 use Symfony\Component\HttpFoundation\Response;
-
+ 
 class Admin
 {
     /**
@@ -17,9 +16,10 @@ class Admin
      */
     public function handle(Request $request, Closure $next): Response
     {
-        if (Auth::user()->usertype != 'admin'){
+        if (Auth::user()->usertype != 'admin') {
             return redirect('dashboard');
         }
+ 
         return $next($request);
     }
 }
